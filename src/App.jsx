94cadbae8ce853140e65dddec1e3fe42053adc87
Dharun Nagavel <dharunnagavel1226@gmail.com>
2025-09-09 20:12:13 +0530

    dependences

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-red-700'>hi this our Tribal Earth 360</h1>
+    </>
+  )
+}
+
+export default App
