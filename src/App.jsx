1e64218579ac9ae7be28b2075a97859f20367db3
Dharun Nagavel <dharunnagavel1226@gmail.com>
2025-09-09 23:05:02 +0530

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'; 
+import {Home} from './pages/Home'; 
+import Map from './pages/Map'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/map' element={<Map />} />
     </Routes>
     </BrowserRouter>
     </>
