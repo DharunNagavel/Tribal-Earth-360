0cdbfac39a0c6abc35fe865a39b1b1edf44ad789
Dharun Nagavel <dharunnagavel1226@gmail.com>
2025-10-03 17:13:07 +0530

    cloud databse neon

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,11 @@
-import { Pool } from 'pg';
+import { DATABASE_URL } from './config/env.js';
+import { Pool } from "pg";
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Tribal 360',
-    password: 'dharun@2005',
-    port: 5432,
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 export default pool;
